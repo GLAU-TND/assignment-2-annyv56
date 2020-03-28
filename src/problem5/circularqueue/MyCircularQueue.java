77d110d499789f0dfb046b12289d5e9a4fec079a
ananya verma <45136882+annyv56@users.noreply.github.com>
2020-03-28 22:27:01 +0530

    Update MyCircularQueue.java
    enqueue dequeue and size method created

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -24,4 +24,44 @@ public class MyCircularQueue {
     }  
     return response;
   }
+  public void enqueue(Student element){ 
+    Node node=new Node(element); 
+    if(front==null){    
+      front=node;
+      rear=node;
+      rear.setNext(front);
+      size++;
+      System.out.println("Value successfully appended");
+    }
+       
+    else{
+       rear.setNext(node);
+       rear=node;
+       rear.setNext(front);
+       size++;
+       System.out.println("Value successfully appended");
+    }
+  }
+  public Node dequeue(){ 
+    Node response=null; 
+    if(front!=null){   
+      if(front.getNext()!=front){    
+        response=new Node(front.getData());     
+        front=front.getNext();    
+        rear.setNext(front);    
+        size--;
+      }
+      else{    
+        response=new Node(front.getData());     
+        front=null;
+        rear=null;
+        size--;
+      }
+    }
+    return response;
+  }
+  public int size(){
+    return size;
+  }
 }
+
