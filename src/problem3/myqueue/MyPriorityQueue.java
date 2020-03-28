7278a604589bf14a7e5ddd3ef1025ca9fa92e42c
ananya verma <45136882+annyv56@users.noreply.github.com>
2020-03-28 21:07:52 +0530

    Update MyPriorityQueue.java
    fields created

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -5,6 +5,11 @@
  *  Time: 7:15 PM
  */
 package problem3.myqueue;
+import problem3.node.Node;
 
 public class MyPriorityQueue {
+   private Node front;
+   private Node rear;
+   private int size;
+
 }
