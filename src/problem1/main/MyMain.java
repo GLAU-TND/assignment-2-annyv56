5fe8ae5e2c151c27c41520437edc2e474f5f7328
ananya verma <45136882+annyv56@users.noreply.github.com>
2020-03-28 20:05:21 +0530

    Update MyMain.java
    bst object for MyBinarySearchTree created

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -4,8 +4,13 @@
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
+
 package problem1.main;
+import java.util.Scanner;
 // executable class
 
 public class MyMain {
+   public static void main(String[] args) { 
+     Scanner sc = new Scanner(System.in);
+     MyBinarySearchTree bst = new MyBinarySearchTree();
 }
