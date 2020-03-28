508705c86b84d6e2931377d33aae71fb8badffcf
ananya verma <45136882+annyv56@users.noreply.github.com>
2020-03-28 20:34:52 +0530

    Update MyMain.java
    method for preorder created

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,5 +7,17 @@
 package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
 public class MyMain {
+  public static void preorder(TreeNode root){    
+    if(root == null)    return;   
+    else{
+            
+      System.out.print(root.getData()+"\t");
+      preorder(root.getLeft());
+      preorder(root.getRight());
+    }
+  }
 }
