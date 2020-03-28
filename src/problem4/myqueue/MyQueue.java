038a5631b48b9c70ec27dd0d0e0a30183fb297ea
ananya verma <45136882+annyv56@users.noreply.github.com>
2020-03-28 22:01:19 +0530

    Update MyQueue.java
    getter setter created

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -10,4 +10,22 @@ package problem4.myqueue;
 public class MyQueue {
   int data;
   Node next;
+  public Node(int data) {   
+    this.data = data;
+    next=null;
+  }
+  public void setData(int data) {   
+    this.data = data;
+  }
+  public void setNext(Node next) {
+    this.next = next;
+  }
+    
+  public int getData() {
+    return data;
+  }
+
+  public Node getNext() {
+    return next;
+  }
 }
