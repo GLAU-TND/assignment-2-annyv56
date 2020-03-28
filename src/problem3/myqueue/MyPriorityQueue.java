6893ff34c07c840845754d70cc0510b00a5a4d1f
ananya verma <45136882+annyv56@users.noreply.github.com>
2020-03-28 11:37:25 +0530

    Update MyPriorityQueue.java
    method for traversing created

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -80,18 +80,31 @@ public class MyPriorityQueue {
                 temp=temp.getNext();
             }
         }
+  }
+  
+  public void traversing(){
+    if(front!=null) {
+      Node temp = front;
+      int temp1=size;
+      while (temp1!=0) {
+        System.out.print(temp.getData() + " ");
+        temp = temp.getNext();
+        temp1--;
+      }
+      
+      System.out.println();
     }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+    
+    else 
+    {
+      System.out.println("sorry! Queue is empty.");
+    }
+    public int getSize() {
+      return size;
+    }
+  }
 }
+
   
                             
                             
