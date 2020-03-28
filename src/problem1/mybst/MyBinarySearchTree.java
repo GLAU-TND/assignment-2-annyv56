0fb30291b06e358e9315156b10104d4698fb35fd
ananya verma <45136882+annyv56@users.noreply.github.com>
2020-03-28 20:12:12 +0530

    Update MyBinarySearchTree.java
    feilds created

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,14 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+import problem1.node.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+  private TreeNode root;
+  private int size;
+  
 }
